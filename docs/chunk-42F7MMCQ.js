93b51ab1aebae54eb2b719a5bd6c6cea7a971cb8
Julian Sanchez <juliansanchezv96@gmail.com>
2024-04-30 01:04:42 -0500

    icons updated

--- docs/chunk-42F7MMCQ.js
+++
@@ -1,1 +0,0 @@
-import{K as a}from"./chunk-L6YER4CC.js";import"./chunk-UUB7ZCBT.js";export{a as default};
---
+++ docs/chunk-BVJMXOPV.js
@@ -0,0 +1,1 @@
+import{K as a}from"./chunk-VNYJ4UYH.js";import"./chunk-UUB7ZCBT.js";export{a as default};
